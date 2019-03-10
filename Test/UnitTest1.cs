77a08e5df098e27242ff0975eacd11a0a00c6a67
Simon Tamas Jozsef <simontjozsef@gmail.com>
2019-03-10 17:10:05 +0100

    Cleaned up project structure

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VeletlenVacsora.Test {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1() {
+		}
+	}
+}
--- VeletlenVacsora/VeletlenVacsora/DependecyServices/ILog.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VeletlenVacsora.DependecyServices {
-	public interface ILog {
-		void MakeLog(string Message, LogType logType = LogType.Info);
-	}
-
-	public enum LogType {Info,Warn,Error}
-}
