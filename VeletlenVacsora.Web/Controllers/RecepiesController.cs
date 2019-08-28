2b579c177ae6c8c70844e1791ed5b76dbfd2f90b
tamas_simon <tamas_simon@SIMON-T2>
2019-08-28 15:46:51 +0200

    added Categories to mockrepository

---
+++ VeletlenVacsora.Web/Controllers/RecepiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VeletlenVacsora.Data;
+using VeletlenVacsora.Web.Services;
+
+namespace VeletlenVacsora.Web.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RecepiesController : ControllerBase {
+		private IVacsoraRepository _repository;
+
+		public RecepiesController(IVacsoraRepository repository) {
+			_repository = repository;
+		}
+
+		[HttpGet]
+		public IEnumerable<Recepie> GetAllRecepies() {
+			var results = _repository.GetAllRecepies();
+			return results;
+		}
+
+
+		[HttpGet("{ID}")]
+		public Recepie GetRecepieByID(int ID) {
+			var result = _repository.GetRecepieByID(ID);
+			return result;
+		}
+	}
+}
