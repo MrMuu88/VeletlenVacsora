610bd40d5984a70d2b6ede4d65bf86f1c14eea79
Tamás Simon <tsimon@geomant.com>
2020-05-14 11:29:27 +0200

    App is fired Up on startup

--- VeletlenVacsora.Web/Startup.cs
+++ VeletlenVacsora.Web/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using VeletlenVacsora.Web.Services;
@@ -8,7 +9,10 @@ namespace VeletlenVacsora.Web {
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services) {
+
+			services.AddControllers();
 			services.AddScoped<IVacsoraRepository, DbVacsoraRepository>();
+			services.AddSpaStaticFiles(configuration => configuration.RootPath="VeletlenVacsoraApp/dist");
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -16,6 +20,30 @@ namespace VeletlenVacsora.Web {
 			if (env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			}
+			app.UseStaticFiles();
+			if (!env.IsDevelopment()) {
+				app.UseSpaStaticFiles();
+			}
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints=>
+				{
+					endpoints.MapControllerRoute(name:"default",pattern:"{controller}/{action=Index}/{id?}");
+				}
+			);
+
+			app.UseSpa(spa =>
+			{
+				// To learn more about options for serving an Angular SPA from ASP.NET Core,
+				// see https://go.microsoft.com/fwlink/?linkid=864501
+				spa.Options.SourcePath = "VeletlenVacsoraApp";
+
+				if (env.IsDevelopment())
+				{
+					spa.UseAngularCliServer(npmScript: "start");
+				}
+			});
 		}
 	}
 }
