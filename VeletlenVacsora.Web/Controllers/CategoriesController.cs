ce6915c9570623dd793a2a129e2fc2f1ea3ba180
Tamás Simon <tsimon@geomant.com>
2020-05-18 06:00:01 +0200

    created Inherent CRUD controllers

--- VeletlenVacsora.Web/Controllers/CategoriesController.cs
+++ VeletlenVacsora.Web/Controllers/CategoriesController.cs
@@ -1,103 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 using VeletlenVacsora.Data.Models;
-using VeletlenVacsora.Web.Services;
-using VeletlenVacsora.Web.Models;
+using VeletlenVacsora.Data.Repositories;
 
-namespace VeletlenVacsora.Web.Controllers {
-	[Route("api/[controller]")]
+namespace VeletlenVacsora.Web.Controllers
+{
 	[ApiController]
-	public class CategoriesController : ControllerBase {
-		//NOTE Delete & Put method on category is not required, insted create a function API to cleanup unsued categories
-		 
-		public IVacsoraRepository Repository { get; set; }
-
-		#region Ctors #############################################################################
-
-		public CategoriesController(IVacsoraRepository repository) {
-			Repository = repository;
+	[Route("Api/[Controller]")]
+	public class CategoriesController : BaseModelController<Category>
+	{
+		public CategoriesController(IRepository<Category> repo) : base(repo)
+		{
 		}
-
-		#endregion
-
-		#region GETs ##############################################################################
-		
-		[HttpGet]
-		public async Task<ActionResult<ICollection<CategoryModel>>> GetCategories(string type = "") {
-			try {
-				var results = new List<CategoryModel>();
-				List<Category> raw;
-				if (!string.IsNullOrWhiteSpace(type)) {
-					raw = (List<Category>)await Repository.GetCategoryByTypeAsync(type);
-				} else {
-					raw = (List<Category>)await Repository.GetAllCategoriesAsync();
-				}
-				foreach (var c in raw) {
-					results.Add(new CategoryModel(c));
-				}
-
-				return Ok(results);
-			} catch (Exception ex) {
-				return StatusCode(StatusCodes.Status500InternalServerError, $"Server Failure: {ex.GetType().Name}\n{ex.Message}");
-			}
-
-
-
-		}
-
-		[HttpGet("{ID}")]
-		public async Task<ActionResult<CategoryModel>> GetCategoryByID(int ID) {
-			try {
-				var c = await Repository.GetCategoryByIDAsync(ID);
-				if (c != null) {
-					return Ok(new CategoryModel(c));
-				} else {
-					return NotFound();
-				}
-
-			} catch (Exception ex) {
-				return StatusCode(StatusCodes.Status500InternalServerError, $"Server Failure: {ex.GetType().Name}\n{ex.Message}");
-			}
-		}
-
-		#endregion
-
-		#region POSTs #############################################################################
-
-		[HttpPost]
-		public async Task<ActionResult<CategoryModel>> PostNew(CategoryModel model) {
-			try {
-
-				if (ModelState.IsValid) {
-
-					CategoryType catType;
-					if (Enum.TryParse(model.Type, true, out catType)) {
-						var newcat = new Category { Name = model.Name, Type = catType };
-						await Repository.Add(newcat);
-						return Created("", new CategoryModel(newcat));
-					}
-
-				}
-
-				return BadRequest();
-
-			} catch (Exception ex) {
-				return StatusCode(StatusCodes.Status500InternalServerError, $"Server Failure: {ex.GetType().Name}\n{ex.Message}");
-			}
-		}
-
-		#endregion
-
-		#region DELETEs ###########################################################################
-		
-		[HttpDelete("Cleanunused")]
-		public async Task<ActionResult> CleanUpUnsued() {
-			return StatusCode(StatusCodes.Status501NotImplemented);
-		}
-
-		#endregion
 	}
 }
--- VeletlenVacsora.Web/Controllers/RecepiesController.cs
+++ VeletlenVacsora.Web/Controllers/RecepiesController.cs
@@ -1,66 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using VeletlenVacsora.Web.Services;
-using VeletlenVacsora.Web.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using VeletlenVacsora.Data.Models;
+using VeletlenVacsora.Data.Repositories;
 
-namespace VeletlenVacsora.Web.Controllers {
-	[Route("api/[controller]")]
+namespace VeletlenVacsora.Web.Controllers
+{
 	[ApiController]
-	public class RecepiesController : ControllerBase {
-		private IVacsoraRepository _repository;
-
-		//TODO Implement Post Method
-		//TODO Implement Put Method
-		//TODO Implement Delete Method
-
-		#region Ctors #############################################################################
-
-		public RecepiesController(IVacsoraRepository repository) {
-			_repository = repository;
+	[Route("Api/[Controller]")]
+	public class RecepiesController : BaseModelController<Recepie>
+	{
+		public RecepiesController(IRepository<Recepie> repo) : base(repo)
+		{
 		}
-
-		#endregion
-
-		#region GETs ##############################################################################
-
-		[HttpGet]
-		public async Task<ActionResult<ICollection<RecepieModel>>> GetRecepies(string Category = "") {
-			try {
-				var results = new List<RecepieModel>();
-				if (!string.IsNullOrWhiteSpace(Category)) {
-					var raw = await _repository.GetRecepiesByTypeAsync(Category);
-
-					foreach (var r in raw) {
-						results.Add(new RecepieModel(r));
-					}
-					return Ok(results);
-				} else {
-					foreach (var recepie in await _repository.GetAllRecepiesAsync()) {
-						results.Add(new RecepieModel(recepie));
-					}
-					return Ok(results);
-				}
-
-			} catch (Exception ex) {
-				return StatusCode(StatusCodes.Status500InternalServerError, $"Server Failure: {ex.GetType().Name}\n{ex.Message}");
-			}
-		}
-
-		[HttpGet("{ID}")]
-		public async Task<ActionResult<RecepieModel>> GetRecepieByID(int ID) {
-			try {
-				var result = await _repository.GetRecepieByIDAsync(ID);
-
-				return Ok(new RecepieModel(result));
-
-			} catch (Exception ex) {
-				return StatusCode(StatusCodes.Status500InternalServerError, $"Server Failure: {ex.GetType().Name}\n{ex.Message}");
-			}
-		}
-
-		#endregion
 	}
 }
