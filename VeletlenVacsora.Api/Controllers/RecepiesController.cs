7fffce38b05e2a5e11c37011bfc16a14fc877f2c
Tamás Simon <tsimon@geomant.com>
2020-12-31 18:58:40 +0100

    Added Endpoint Api/Recepies/GetRandom

--- VeletlenVacsora.Api/Controllers/RecepiesController.cs
+++ VeletlenVacsora.Api/Controllers/RecepiesController.cs
@@ -1,7 +1,12 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 using VeletlenVacsora.Api.ViewModels;
+using VeletlenVacsora.Data.Extensions;
 using VeletlenVacsora.Data.Models;
 using VeletlenVacsora.Data.Repositories;
 
@@ -13,6 +18,29 @@ namespace VeletlenVacsora.Api.Controllers
 	{
 		public RecepiesController(ILogger<RecepiesController> logger,IRepository<RecepieModel> repo, IMapper mapper) : base(logger,repo, mapper)
 		{
+		}
+
+		/// <summary>
+		/// Returns a Recepie based on Weightened random selection
+		/// </summary>
+		[HttpGet]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(500)]
+		public async Task<ActionResult<Recepie>> GetRandom()
+		{
+			try
+			{
+				var recepie = await Repository.GetRandomAsync();
+				return Ok(recepie);
+
+			}
+			catch (Exception ex)
+			{
+				var methodInfo = MethodBase.GetCurrentMethod();
+				logger.LogError(ex, $"An Exception occured in {methodInfo.DeclaringType.Name}.{methodInfo.Name}");
+				var errorobj = new { Error = ex.GetType().Name, ex.Message };
+				return StatusCode(StatusCodes.Status500InternalServerError, errorobj);
+			}
 		}
 
 
