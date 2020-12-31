27820415722bb71ad308f4f443e6d9f848d0ae8e
Tamás Simon <tsimon@geomant.com>
2020-12-31 17:54:11 +0100

    added migrations

---
+++ VeletlenVacsora.Data/Migrations/20201231164403_Set_default_2_recepieWeight.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VeletlenVacsora.Data.Migrations
+{
+    public partial class Set_default_2_recepieWeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Weight",
+                table: "Recepies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Weight",
+                table: "Recepies",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldDefaultValue: 0);
+        }
+    }
+}
