922bc6395bd042f826039625c5a0df431c79932f
Tamás Simon <tsimon@geomant.com>
2021-01-02 17:33:04 +0100

    added Default objects for related entities

--- VeletlenVacsora.Data/Models/IngredientModel.cs
+++ VeletlenVacsora.Data/Models/IngredientModel.cs
@@ -3,13 +3,13 @@
 namespace VeletlenVacsora.Data.Models {
 	public class IngredientModel :BaseModel{
 
-		public CategoryModel IngredientType { get; set; }
+		public CategoryModel IngredientType { get; set; } = new CategoryModel(){Type = CategoryType.Ingredient};
 
-		public CategoryModel PackageType { get; set; }
+		public CategoryModel PackageType { get; set; } = new CategoryModel() { Type = CategoryType.Package};
 
 		public int Price { get; set; }
 
-		public ICollection<RecepieIngredientModel> Recepies { get; set; }
+		public ICollection<RecepieIngredientModel> Recepies { get; set; } = new List<RecepieIngredientModel>();
 
 		public IngredientModel(string name):base(name) { }
 		public IngredientModel() { }
--- VeletlenVacsora.Data/Models/RecepieModel.cs
+++ VeletlenVacsora.Data/Models/RecepieModel.cs
@@ -3,10 +3,10 @@
 namespace VeletlenVacsora.Data.Models {
 	public class RecepieModel:BaseModel {
 		public int Weight { get; set; }
-		public CategoryModel Category { get; set; }
+		public CategoryModel Category { get; set; } = new CategoryModel() { Type=CategoryType.Recepie};
 		public int? OnMenu { get; set; }
 
-		public ICollection<RecepieIngredientModel> Ingredients { get; internal set; }
+		public ICollection<RecepieIngredientModel> Ingredients { get; internal set; } = new List<RecepieIngredientModel>();
 
 
 		public RecepieModel(string name):base(name) { }
