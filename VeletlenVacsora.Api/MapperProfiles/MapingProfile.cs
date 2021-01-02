ac8a13c2b4f2fce671c64f44f7f6dfbd4ff1b7f8
Tamás Simon <tsimon@geomant.com>
2021-01-02 17:34:20 +0100

    modified Mapping Profile

--- VeletlenVacsora.Api/MapperProfiles/MapingProfile.cs
+++ VeletlenVacsora.Api/MapperProfiles/MapingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using VeletlenVacsora.Api.ViewModels;
 using VeletlenVacsora.Data.Models;
+using VeletlenVacsora.Data.Repositories;
 
 namespace VeletlenVacsora.Api.MapperProfiles
 {
@@ -9,14 +10,18 @@ namespace VeletlenVacsora.Api.MapperProfiles
 		public MapingProfile()
 		{
 			CreateMap<CategoryModel, Category>().ReverseMap();
+			
 			CreateMap<RecepieModel, Recepie>()
-				.ForMember(r=>r.Category,rm=>rm.MapFrom(rm=>rm.Category.Name))
-				.ReverseMap();
+				.ForMember(r => r.Category, rm => rm.MapFrom(rm => rm.Category.Name));
+			CreateMap<Recepie, RecepieModel>()
+				.ForMember(r => r.Category, rm => rm.Ignore());
 
-			CreateMap<IngredientModel,Ingredient>()
-				.ForMember(i => i.IngredientType,im=> im.MapFrom(im=> im.IngredientType.Name))
-				.ForMember(i => i.PackageType,im=> im.MapFrom(im=> im.PackageType.Name))
-				.ReverseMap();
+			CreateMap<IngredientModel, Ingredient>()
+				.ForMember(i => i.IngredientType, im => im.MapFrom(im => im.IngredientType.Name))
+				.ForMember(i => i.PackageType, im => im.MapFrom(im => im.PackageType.Name));
+			CreateMap<Ingredient, IngredientModel>()
+				.ForMember(i => i.IngredientType, im => im.Ignore())
+				.ForMember(i => i.PackageType, im => im.Ignore());
 
 			CreateMap<RecepieIngredientModel,RecepieIngredient>()
 				.ForMember(ri=>ri.Ingredient,rim=>rim.MapFrom(rim=> rim.Ingredient.Name))
@@ -24,5 +29,6 @@ namespace VeletlenVacsora.Api.MapperProfiles
 				.ForMember(ri=>ri.IngredientType,rim=>rim.MapFrom(rim=> rim.Ingredient.IngredientType.Name))
 				.ReverseMap();
 		}
+
 	}
 }
