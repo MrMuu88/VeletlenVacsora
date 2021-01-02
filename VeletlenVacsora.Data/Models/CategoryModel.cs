480c0a8be205fe55bf6b7def6f6b688e8e06f3c9
Tamás Simon <tsimon@geomant.com>
2021-01-02 17:32:25 +0100

    added Type to Categorymodel ctor

--- VeletlenVacsora.Data/Models/CategoryModel.cs
+++ VeletlenVacsora.Data/Models/CategoryModel.cs
@@ -2,7 +2,9 @@
 	public class CategoryModel :BaseModel{
 		public CategoryType Type { get; set; }
 
-		public CategoryModel(string name) : base(name) { }
+		public CategoryModel(string name,CategoryType type = CategoryType.Ingredient) : base(name) {
+			Type = type;
+		}
 		public CategoryModel() { }
 
 	}
